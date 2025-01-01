db911742d4bda162f02d589620a63b33887cb873
lasanga890 <lasangad890@gmail.com>
2025-01-02 00:04:58 +0530

    Fix errors in use index to identify

--- backend/controllers/User/DocumentsVerify/olCertificateVerify.js
+++ backend/controllers/User/DocumentsVerify/olCertificateVerify.js
@@ -2,10 +2,16 @@ export const olCertificateVerify = async (req, res) => {
   try {
     const { extractedText } = req;
     const { Index_OL } = req.body;
+    console.log(Index_OL);
     if (!extractedText) {
       return res
         .status(400)
         .send({ success: false, message: "No text provided for analysis" });
+    }
+    if (!Index_OL) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Index_OL field is required" });
     }
     const lowerCaseExtractedText = extractedText.toLowerCase();
     console.log(lowerCaseExtractedText);
@@ -15,9 +21,8 @@ export const olCertificateVerify = async (req, res) => {
     const matches = predefinedTexts.filter((text) =>
       lowerCaseExtractedText.includes(text)
     );
-
-    const index = Index_OL.filter((text) =>
-      lowerCaseExtractedText.includes(text)
+    const index = lowerCaseExtractedText.includes(
+      Index_OL.toString().toLowerCase()
     );
 
     matches.length > 0 && index
@@ -25,6 +30,7 @@ export const olCertificateVerify = async (req, res) => {
           success: true,
           message: "Verified OL Certificate",
           matches,
+          index,
         })
       : res.status(200).send({
           success: false,
