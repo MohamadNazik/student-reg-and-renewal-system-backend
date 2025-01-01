8527e9b75916571f9d3c7602276c9d24812c2af0
Sumy2001 <154615028+Sumy2001@users.noreply.github.com>
2025-01-01 18:42:18 +0000

    create HostelAccommodationFormVerify.js

---
+++ backend/controllers/User/DocumentsVerify/HostelAccommodationFormVerify.js
@@ -0,0 +1,40 @@
+export const HostelAccommodationFormVerify = async (req, res) => {
+    try {
+      const { extractedText } = req;
+      if (!extractedText) {
+        return res
+          .status(400)
+          .send({ success: false, message: "No text provided for analysis" });
+      }
+  
+      // Convert extracted text to lowercase
+      const lowerCaseExtractedText = extractedText.toLowerCase();
+      console.log(lowerCaseExtractedText);
+  
+      // Define predefined texts in lowercase
+      const predefinedTexts = ["hostel accommodation approval", "warden approval signature", "university seal"];
+  
+      // Check for matches
+      const matches = predefinedTexts.filter((text) =>
+        lowerCaseExtractedText.includes(text)
+      );
+  
+      matches.length > 0
+        ? res.status(200).send({
+            success: true,
+            message: "Verified hostel accommodation form ",
+            matches,
+          })
+        : res.status(200).send({
+            success: false,
+            message: "Not verified",
+            matches,
+          });
+    } catch (error) {
+      res.status(500).send({
+        success: false,
+        message: "Internal Server Error",
+      });
+    }
+  };
+  
